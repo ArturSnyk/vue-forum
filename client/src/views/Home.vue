cceff9373ee17e23bf78141e989ddb8f6eaedd24
Artur Kotliar <artur@snyk.io>
2019-10-04 17:40:30 +0300

    first commit

---
+++ client/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div class="jumbotron">
+      <h1 class="display-3">Welcome to me practicing vue</h1>
+      <p class="lead">Lets see what we can do with vue 🤩</p>
+      <hr class="my-4">
+      <p>It's all about practicing some code.</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      </p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+};
+</script>
